85b54a3211c17cb68b40d07d3b232ea609f5746b
Knut Edvard Kjersem <knutekjersem@gmail.com>
2024-05-28 14:48:36 +0200

    Hello

---
+++ masifAPI/Controllers/PhotoUploadController.cs
@@ -0,0 +1,16 @@
+using masifAPI.Data;
+using masifAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace masifAPI.Controllers;
+
+    public class PhotoUploadController : ControllerBase{
+
+      
+}
+        
+
+        
+    
