45510d3e9f333d00277f83db7a400671a56b1aa5
Knut Edvard Kjersem <knutekjersem@gmail.com>
2024-07-24 22:38:12 +0200

    yes

--- obj/Debug/net8.0/masifAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/masifAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("masifAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aa05ae94fe991525de611cd89e193f0f951f4cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9d03351614ca50fd31dacf471156e9838baf9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("masifAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("masifAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
